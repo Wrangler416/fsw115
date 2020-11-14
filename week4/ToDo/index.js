52f5e028d7c7dff655f8419fab3e0c9235e24d05
Kara Temple <kara.temple@bryanuniversity.edu>
2020-11-14 16:52:20 -0500

    assignment1

--- week4/ToDo/index.js
+++ week4/ToDo/index.js
@@ -8,15 +8,17 @@ axios.get("https://api.vschool.io/karatemple/todo").then(response => {
 
         let input = document.createElement("input")
         input.setAttribute("type", "checkbox")
-        input.onclick= crossout()
+        input.setAttribute("id", "clicked")
         document.body.appendChild(input) 
 
-        function crossout() {
-            let ele = document.getElementsByClassName("myelement");
-            for(var x = 0; x < ele.length; x++){
-            ele[i].style.setProperty("text-decoration", "line-through");
-    }
-        } 
+        let clicked = document.getElementById("clicked")
+        clicked.addEventListener("click", function() {
+
+            for(var x = 0; x < h1.length; x++){
+                h1[i].style.setProperty("text-decoration", "line-through")
+            }
+        })
     }
 })
-.catch()
+
+.catch() 
